4a9677923613ab1cef1fbc2254d1ffcc3e4b2795
Cody Saylor <saylorc1@oit-tag09509.local>
2020-10-03 10:30:29 -0400

    fixing stackbit build command

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
-const ampPlugin = require('@ampproject/eleventy-plugin-amp');
-
 module.exports = function (eleventyConfig) {
   const opts = {
     dir: {
       input: "content",
-      // includes: "../components"
+      includes: "../components"
     }
   };
 
